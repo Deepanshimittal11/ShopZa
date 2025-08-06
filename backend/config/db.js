25f31bb23572b6aa2337ef16ba4da6cf5c22072a
Deepanshi Mittal <your_email@example.com>
2025-08-06 18:22:40 +0530

    Added user registration, login, and profile functionality

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+//this file will connect our application of mongodb to database.
+const mongoose = require("mongoose");
+
+const connectDB = async() => {
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB connected successfully!");
+    }catch(err){
+        console.error("MongoDB connection failed.", err);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB
